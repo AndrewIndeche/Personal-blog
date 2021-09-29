4e345e63d55780c21369880392663b69daf6f71b
AndrewIndeche <indecheandrew@gmail.com>
2021-09-29 19:35:30 +0300

    add configurations for users and api

--- config.py
+++ config.py
@@ -1,2 +1,26 @@
 import os
     QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+        #email config
+class Config:
+    '''
+    '''
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProductionConfig(Config):
+    '''
+    '''
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
+
+class DevelopmentConfig(Config):
+    '''
+    '''
+    DEBUG=True
+
+config_options={
+    'dev': DevelopmentConfig,
+    'prod': ProductionConfig
+}
