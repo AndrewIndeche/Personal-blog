82e98da49d1545472ce38d5772306b427f9668fe
AndrewIndeche <indecheandrew@gmail.com>
2021-09-29 14:01:50 +0300

    Add main route

---
+++ app/main/views.py
@@ -0,0 +1,10 @@
+@app.route('/')
+def index(id):
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('index.html')
